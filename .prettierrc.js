837ebc566ceac78df3f85a488e50706482099407
Wes Pearce <wespearce1@gmail.com>
2021-04-07 06:14:53 -0600

    Eslint/prettier setup

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 // prettier.config.js or .prettierrc.js
 module.exports = {
-    trailingComma: 'es5',
-    tabWidth: 2,
-    semi: true,
-    singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
 };
--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return (
-        <div>
-            <h1>hello world</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>hello world</h1>
+    </div>
+  );
 }
 
 export default App;
